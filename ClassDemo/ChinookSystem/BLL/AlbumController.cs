7217668d116e23c19bb1c157d66c57e09cbae078
tblakely2 <tblakely2@studentmail.nait.ca>
2017-10-18 20:50:21 -0600

    ?
    ...?

--- ClassDemo/ChinookSystem/BLL/AlbumController.cs
+++ ClassDemo/ChinookSystem/BLL/AlbumController.cs
@@ -44,5 +44,75 @@ namespace ChinookSystem.BLL
             return results.ToList();
         }
     }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Album> Albums_ListByTitle(string title)
+        {
+            using (var context = new ChinookContext())
+            {
+                var results = from x in context.Albums
+                              where x.Title.Contains(title)
+                              orderby x.Title, x.ReleaseYear
+                              select x;
+                return results.ToList();
+            }
+        }//eom
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Album> Albums_List()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Album Albums_Get(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+
+
+        [DataObjectMethod(DataObjectMethodType.Insert,false)]
+        public int Albums_Add(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                item = context.Albums.Add(item); //staging
+                context.SaveChanges(); //commit of the request
+                return item.AlbumId;
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Update,false)]
+        public int Albums_Update(Album item)
+        {
+            using (var context = new ChinookContext())
+            {
+                context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+                return context.SaveChanges();
+            }
+        }
+        
+        public int Albums_Delete(int albumid)
+        {
+            using (var context = new ChinookContext())
+            {
+                var existingItem = context.Albums.Find(albumid);
+                context.Albums.Remove(existingItem);
+                return context.SaveChanges();
+
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Delete,false)]
+        public int Albums_Delete(Album item)
+        {
+            return Albums_Delete(item.AlbumId);
+        }
     }
 }
